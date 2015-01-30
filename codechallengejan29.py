b2823d8f397226086842dd8603b109e52379de57
Your Name <you@example.com>
2015-01-29 19:42:16 -0800

    init commit

---
+++ codechallengejan29.py
@@ -0,0 +1,22 @@
+ # The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+#
+# This is the start of the function to get you started.
+
+def sleep_in(weekday, vacation):
+    if weekday == True:
+        print
+
+
+
+
+
+# sleep_in(False, False) # Should return True
+# sleep_in(True, False) # Should return False
+# sleep_in(False, True) # Should return True
+
+################################################################
+# 2. Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+#
+# This is the start of the function to get you started.
+
+# def hello_name(name):
--- phonebook.py
+++ phonebook.py
@@ -1,4 +1,4 @@
-phonebook = {'chris':{'name':'Chris', 'phone': '555-555-5555'}, 'mg':{'name':'MG', 'phone': '666-666-6666'}}
+phonebook = {'chris': {'name': 'Chris', 'phone': '555-555-5555'}, 'mg': {'name': 'MG', 'phone': '666-666-6666'}}
 #python chooses the order of display that suits it best.
 for x in phonebook:
     print phonebook[x]
@@ -8,6 +8,7 @@ print "search"
 print "add"
 print "edit"
 print "remove"
+print "exit"
 
 choose = raw_input("I would like to: ")
 
@@ -21,13 +22,30 @@ elif choose == "add":
     phonebook.update({add_name:{'name': add_name, 'phone': add_phone }})
     print phonebook
 elif choose == "edit":
-    edit_choose = raw_input("Enter name: ")
-    edit_name = raw_input("Change name to: ")
-    edit_phone = raw_input("Change phone to: ")
-    phonebook[edit_name] = phonebook[edit_choose]
-    phonebook.update({edit_name:{'name': edit_name, 'phone': edit_phone}})
+    edit_choose = raw_input("Enter name: ").lower()
+    choice_edit = raw_input("what would you like to edit? Name or Phone or both?: ")
+    if choice_edit == "name".lower():
+        edit_name = raw_input("Change name to: ")
+        phonebook[edit_choose]['name'] = edit_name
+        phonebook[edit_name] = phonebook[edit_choose]
+        phonebook.pop(edit_choose)
+        print phonebook
+    elif choice_edit == "phone".lower():
+        edit_phone = raw_input("Change phone to: ")
+        phonebook[edit_choose]['phone'] = edit_phone
+        print phonebook
+
+    else:
+        edit_name = raw_input("Change name to: ")
+        edit_phone = raw_input("Change phone to: ")
+        phonebook[edit_name] = phonebook[edit_choose]
+        phonebook.update({edit_name:{'name': edit_name, 'phone': edit_phone}})
+        phonebook.pop(edit_choose)
+
     print phonebook
 elif choose == "remove":
     remove_choose = raw_input("enter name to remove: ")
     phonebook.pop(remove_choose)
     print phonebook
+else:
+    exit()
