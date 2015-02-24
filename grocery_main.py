e071bfa6c054e69c9280f043b123204d2c00e119
Ryan Grover <ryangrover113@gmail.com>
2015-02-23 21:00:51 -0800

    added __str__
    redefined print
    
    example print(ryan)

--- grocery_main.py
+++ grocery_main.py
@@ -1,4 +1,3 @@
-# yo mikey!!!!
 # Here is our initial list of items for our store. This will eventually live in another file once we
 # have set up a few more of the basic items.
 items = {apples: {price: 1, qyt: 5}, kashi: {price: 2, qty:15}, mops: {price: 5, qty: 20}}
