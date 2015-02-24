eebbc5fa5a24a95e3b444c21e2439a3d73eed15b
Ryan Grover <ryangrover113@gmail.com>
2015-02-23 20:57:58 -0800

    add def __str__
    redifened str to print out customer credentials.
    
    example: print(ryan)

--- grocery_main.py
+++ grocery_main.py
@@ -1,17 +1,25 @@
+# yo mikey!!!!
 # Here is our initial list of items for our store. This will eventually live in another file once we
 # have set up a few more of the basic items.
 items = {apples: {price: 1, qyt: 5}, kashi: {price: 2, qty:15}, mops: {price: 5, qty: 20}}
-
 # This will create a basic profile for users. Later in the program buyers/sellers will be distinguished
+
+
 class Profile(object):
-  def __init__(self, first_name, last_name, money, password)
+  def __init__(self, first_name, last_name, money, password):
     self.first_name = first_name
     self.last_name = last_name
     self.money = money
     self.password = password
 
+  def __str__(self):
+    return 'Customer: {0} {1}, Money: ${2}, Password: {3}'.format(self.first_name, self.last_name, self.money, self.password)
+
 
 class Buyer(Profile):
-  def Cart(self):
-    self.inventory = { }
+    pass
+
+   def Cart(self):
+    self.inventory = {}
     
+ # we are gonna rock!
